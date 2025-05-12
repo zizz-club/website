876e74d9b7f408aa7e021b166036f7e9ff70c622
grej <mathias-toft@live.dk>
2025-05-12 12:03:28 +0200

    Update navbar: change Panel link to absolute URL

--- modules/navbar.php
+++ modules/navbar.php
@@ -3,6 +3,6 @@
         <li><a href="?page=home" data-page="home">Home</a></li>
         <li><a href="?page=about" data-page="about">About</a></li>
             <!-- External Links -->
-        <li><a href="/panel">Panel</a></li>
+        <li><a href="https://panel.zizz.club/">Panel</a></li>
     </ul>
 </nav>
