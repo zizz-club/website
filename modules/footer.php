19d10849de5b6b42d5ef104529e67f68114ac26b
grej <mathias-toft@live.dk>
2025-05-12 20:21:54 +0200

    Update footer: change GitHub link to point to the correct repository

--- modules/footer.php
+++ modules/footer.php
@@ -1,4 +1,4 @@
 <footer>
     <div class="content"><?php echo date("Y"); ?> zizz.club. all rights reserved</div>
-    <div class="content">site github: <a href="https://github.com/zizz-club/zizz-club_site">zizz-club/zizz-club_site</a></div>
+    <div class="content">site github: <a href="https://github.com/zizz-club/website">zizz-club/website</a></div>
 </footer>
