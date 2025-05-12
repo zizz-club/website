d675e05eb8c90de06e79e9844dcbafe35895341c
grej <mathias-toft@live.dk>
2025-05-12 12:41:22 +0200

    Refactor home page content and remove unused panel file

--- pages/home.php
+++ pages/home.php
@@ -5,7 +5,7 @@
 <div class="wrapper">
     <div class="box">
         <h1>Welcome to zizz.club.</h1>
-        <p>The site is currently being developed. Stay tuned for updates!</p>
-        <p>In the meantime, feel free check the owners site <b>@ <a href="https://grej.xyz">grej.xyz</a></b></p>
+        <p>The site is currently being developed.</p>
+        <p>In the meantime, feel free to check the owner's site <b>@ <a href="https://grej.xyz">grej.xyz</a></b></p>
     </div>
 </div>
