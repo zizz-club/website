69981198673ba2a13f3973c5b50a5a1114a556d8
grej <mathias-toft@live.dk>
2025-05-12 07:01:14 +0200

    Add navbar component and styles, update index.php structure, and enhance content presentation

--- index.php
+++ index.php
@@ -7,12 +7,12 @@
 </head>
     <body>
         <div id="threejs-container"></div>
+        <?php include 'modules/navbar.php'; ?>
         <div class="wrapper">
             <div class="content">
-                <h1>zizz.club</h1>
-                <p>Welcome to zizz.club.</p>
+                <h1>Welcome to zizz.club.</h1>
                 <p>We are currently working on the site. Stay tuned for updates!</p>
-                <p>In the meantime, feel free check the owners site <a href="https://grej.xyz">@grej.xyz</a></p>
+                <p>In the meantime, feel free check the owners site <b>@ <a href="https://grej.xyz">grej.xyz</a></b></p>
         </div>
         <?php include 'assets/js/imports.php'; ?>
     </body>
---
+++ modules/navbar.php
@@ -0,0 +1,6 @@
+<nav id="navbar">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="/panel">Panel</a></li>
+    </ul>
+</nav>
