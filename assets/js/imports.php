a820a4ebdc279b3e536cb02872bda7fdb9040ebd
grej <mathias-toft@live.dk>
2025-05-12 06:38:56 +0200

    Add initial CSS and JavaScript files, implement Three.js background, and update index.php structure

---
+++ assets/js/imports.php
@@ -0,0 +1,3 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/simplex-noise/2.4.0/simplex-noise.min.js"></script>
+<script type="module" src="assets/js/modules/background.js"></script>
--- index.php
+++ index.php
@@ -3,8 +3,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>zizz.club</title>
+    <link rel="stylesheet" href="assets/css/imports.css">
 </head>
-<body>
-    <div id="threejs-container" style="width: 100%; height: 100vh;"></div>
-</body>
+    <body>
+        <div id="threejs-container"></div>
+        <div class="wrapper">
+            <div class="content">
+                <h1>zizz.club</h1>
+                <p>Welcome to zizz.club.</p>
+                <p>We are currently working on the site. Stay tuned for updates!</p>
+                <p>In the meantime, feel free check the owners site <a id="link" href="https://grej.xyz">@grej.xyz</a></p>
+        </div>
+        <?php include 'assets/js/imports.php'; ?>
+    </body>
 </html>
